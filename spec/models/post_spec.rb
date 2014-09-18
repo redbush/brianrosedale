cb50462582c83fd2452e1d1c2ea648a0a59d8b2e
Brian Rosedale <brian.j.rosedale@gmail.com>
2014-09-17 21:18:01 -0700

    add Post presence validations for title and content

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
+  context 'validations' do
+
+    it 'does not save when no title' do
+      post = FactoryGirl.build(:post, title: '')
+
+      expect(post).to_not be_valid
+    end
+
+    it 'does not save when no content' do
+      post = FactoryGirl.build(:post, content: '')
+
+      expect(post).to_not be_valid
+    end
+
+  end
+
 end
