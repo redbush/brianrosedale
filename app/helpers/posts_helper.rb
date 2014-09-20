f6f61915ebbaea0a3a4148c184ecf3a2a5197ca5
Brian Rosedale <brian.j.rosedale@gmail.com>
2014-09-20 11:56:36 -0700

    add post helper for formatting date time

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+
+  def format_datetime(dt)
+    formated_dt = 'Unknown'
+    begin
+      formated_dt = dt.strftime('%m/%d/%Y')
+    rescue NoMethodError
+      formated_dt
+    end
+  end
+
 end
---
+++ spec/helpers/posts_helper_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe PostsHelper do
+
+  context '#format_date' do
+
+    it 'returns formats datetime' do
+      dt = DateTime.new(2014, 9, 20)
+      expected_datetime = '09/20/2014'
+
+      expect(helper.format_datetime(dt)).to eq(expected_datetime)
+    end
+
+    it 'returns empty string when invalid date' do
+      dt = 'baddate'
+
+      expect(helper.format_datetime(dt)).to eq('Unknown')
+    end
+
+  end
+
+end
