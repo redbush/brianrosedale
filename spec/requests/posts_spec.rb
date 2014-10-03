0456436187580e78aa6738edbd316f38a2b91dec
Brian Rosedale <brian.j.rosedale@gmail.com>
2014-10-02 17:27:40 -0700

    add layout for post page, update posts request specs for 404 and templates

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,12 +10,14 @@ RSpec.describe 'Posts', type: :request do
       get posts_path
 
       expect(response.status).to be(200)
+      expect(response).to render_template(:index)
     end
 
     it '/posts/page/1 returns success' do
       get 'posts/page/1'
 
       expect(response.status).to be(200)
+      expect(response).to render_template(:index)
     end
 
   end
@@ -26,6 +28,14 @@ RSpec.describe 'Posts', type: :request do
       get post_path(post)
 
       expect(response.status).to be(200)
+      expect(response).to render_template(:show)
+    end
+
+    it 'not found redirects to not found' do
+      get post_path(123)
+
+      expect(response.status).to eql(404)
+      expect(response).to render_template('404')
     end
 
   end
@@ -33,7 +43,7 @@ RSpec.describe 'Posts', type: :request do
   context 'GET /posts_redirect' do
 
     it 'redirects to posts' do
-      get '/posts_redirect'
+      get posts_redirect_path
 
       expect(response).to redirect_to(posts_path)
     end
